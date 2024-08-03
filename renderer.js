9d17af273d8f79eed1d92e1cfd5614c7a03d4ea3
GodWindows <kanlinsougodwin@gmail.com>
2024-08-03 22:05:07 +0200

    finished the tuto's integrations

---
+++ renderer.js
@@ -0,0 +1,7 @@
+const func = async () => {
+    const response = await versions.ping()
+}
+
+func()
+const information = document.getElementById('info')
+information.innerText = `This app is using Chrome (v${versions.chrome()}), Node.js (v${versions.node()}), and Electron (v${versions.electron()}). By the way, my name is ${versions.my_name}`
